2dd603654365f55bd553ec59c9e80f623850035d
Mustafa Shady <115884192+Mustafa-Shady@users.noreply.github.com>
2023-05-06 13:22:47 +0300

    New Files

---
+++ RCC/include/RCC_cfg.h
@@ -0,0 +1,21 @@
+/*
+ * RCC_cfg.h
+ *
+ *  Created on: May 4, 2023
+ *      Author: 20115
+ */
+
+#ifndef RCC_CFG_H_
+#define RCC_CFG_H_
+/*This Macro is to Configure System Clock Soure */
+/*Available Configrations :
+*HSI 
+*HSE_RC
+*HSE_CRYSTAL
+*PLL
+*/
+#define SYSYTEM_CLK_SRC     HSI
+
+
+
+#endif /* RCC_CFG_H_ */
---
+++ RCC/src/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ *  Created on: May 4, 2023
+ *      Author: 20115
+ */
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+#include "RCC_inteface.h"
+
+int main (void)
+{
+
+}
+
+
+
+
