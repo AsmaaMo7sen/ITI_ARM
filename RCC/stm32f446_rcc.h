9ccada7f477656b969cf5f85f7094c2b1ce8b83a
Mohamed Ashraf <m7mda4f.ma@gmail.com>
2023-05-07 15:36:57 +0300

    Added Macros

--- RCC/stm32f446_rcc.h
+++ RCC/stm32f446_rcc.h
@@ -17,6 +17,51 @@
 #define HSE_CRYS	3
 #define PLL			4
 
+#define AHB1_GPIOA		0
+#define AHB1_GPIOB		1
+#define AHB1_GPIOC		2
+#define AHB1_GPIOD		3
+#define AHB1_GPIOE		4
+#define AHB1_GPIOF		5
+#define AHB1_GPIOG		6
+#define AHB1_GPIOH		7
+#define AHB1_CRC		12
+#define AHB1_DMA1		21
+#define AHB1_DMA2		22
+
+#define AHB2_OTGFS		7
+
+#define APB1_TIM2		0
+#define APB1_TIM3		1
+#define APB1_TIM4		2
+#define APB1_TIM5		3
+#define APB1_TIM6		4
+#define APB1_TIM7		5
+#define APB1_TIM12		6
+#define APB1_TIM13		7
+#define APB1_TIM14		8
+#define APB1_WWDG		11
+#define APB1_SPI1		14
+#define APB1_SPI2		15
+#define APB1_USART2		17
+#define APB1_USART3		18
+#define APB1_USART4		19
+#define APB1_USART5		20
+#define APB1_I2C1		21
+#define APB1_I2C2		22
+#define APB1_I2C3		23
+
+#define APB2_TIM1		0
+#define APB2_TIM8		1
+#define APB2_USART1		4
+#define APB2_USART6		5
+#define APB2_ADC1		8
+#define APB2_ADC2		9
+#define APB2_SPI1		12
+#define APB2_SPI4		13
+
+
+
 
 
 #define AHB_PRESCALER_1		0
