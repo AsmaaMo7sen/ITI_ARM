715b6365c26f6f69df37092fb4d15b0145165090
Mohamed Ashraf <m_ashraaf@outlook.com>
2023-05-09 01:02:35 +0300

    Uploaded RCC Driver to Mohamed_Ashraf_Abdelhamid Branch

---
+++ RCC/main.c
@@ -0,0 +1,20 @@
+/***************************************************************************/
+/*Author      : Mohamed Ashraf Abdelhamid							       */
+/*File Name   : main.c     		                                           */
+/*Bus 	      : AHB1										  	           */
+/*Description : This file contains RCC Application						   */
+/*Date        : 5 May 2023	                                               */
+/***************************************************************************/
+
+#include "STD_LIB.h"
+#include "BIT_MATHS.h"
+
+#include "RCC_interface.h"
+
+void main(void)
+{
+	while(1)
+	{
+
+	}
+}
