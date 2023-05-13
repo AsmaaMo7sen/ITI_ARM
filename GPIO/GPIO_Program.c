4e1d5a5f3a56f77018740d9922b57f6e93b28b54
Mohnd <Mohndelhossiny2040@gmail.com>
2023-05-13 18:06:12 +0300

    update some feature

--- GPIO/GPIO_Program.c
+++ GPIO/GPIO_Program.c
@@ -24,33 +24,33 @@ void GPIO_voidInitOutputPin(u8 Copy_u8PortID,u8 Copy_u8PinID,u8 Copy_u8PinTYPES,
 		GPIOA->OSPEEDR &=~(0b11<<(Copy_u8PinID*2));
 		GPIOA->OSPEEDR|=(Copy_u8PinSpeed<<(Copy_u8PinID*2));
 		/*Set type of output pin*/
-         WR_BIT(GPIOA->OTYPER,Copy_u8PinID,Copy_u8PinTYPES);
+		WR_BIT(GPIOA->OTYPER,Copy_u8PinID,Copy_u8PinTYPES);
 
 		break;
 	case GPIO_PORTB:
 		/*set the direction of the pin to be output*/
-				/*bit masking*/
-				GPIOB->ModeR &=~(3<<(Copy_u8PinID*2));
-				GPIOB->ModeR |=(0b01<<(Copy_u8PinID*2));
-				/*set the SPEED of the pin to be output*/
-				/*bit masking*/
-				GPIOB->OSPEEDR &=~(0b11<<(Copy_u8PinID*2));
-				GPIOB->OSPEEDR|=(Copy_u8PinSpeed<<(Copy_u8PinID*2));
-				/*Set type of output pin*/
-		         WR_BIT(GPIOB->OTYPER,Copy_u8PinID,Copy_u8PinTYPES);
+		/*bit masking*/
+		GPIOB->ModeR &=~(3<<(Copy_u8PinID*2));
+		GPIOB->ModeR |=(0b01<<(Copy_u8PinID*2));
+		/*set the SPEED of the pin to be output*/
+		/*bit masking*/
+		GPIOB->OSPEEDR &=~(0b11<<(Copy_u8PinID*2));
+		GPIOB->OSPEEDR|=(Copy_u8PinSpeed<<(Copy_u8PinID*2));
+		/*Set type of output pin*/
+		WR_BIT(GPIOB->OTYPER,Copy_u8PinID,Copy_u8PinTYPES);
 
 		break;
 	case GPIO_PORTC:
 		/*set the direction of the pin to be output*/
-				/*bit masking*/
-				GPIOC->ModeR &=~(3<<(Copy_u8PinID*2));
-				GPIOC->ModeR |=(0b01<<(Copy_u8PinID*2));
-				/*set the SPEED of the pin to be output*/
-				/*bit masking*/
-				GPIOC->OSPEEDR &=~(0b11<<(Copy_u8PinID*2));
-				GPIOC->OSPEEDR|=(Copy_u8PinSpeed<<(Copy_u8PinID*2));
-				/*Set type of output pin*/
-		         WR_BIT(GPIOC->OTYPER,Copy_u8PinID,Copy_u8PinTYPES);
+		/*bit masking*/
+		GPIOC->ModeR &=~(3<<(Copy_u8PinID*2));
+		GPIOC->ModeR |=(0b01<<(Copy_u8PinID*2));
+		/*set the SPEED of the pin to be output*/
+		/*bit masking*/
+		GPIOC->OSPEEDR &=~(0b11<<(Copy_u8PinID*2));
+		GPIOC->OSPEEDR|=(Copy_u8PinSpeed<<(Copy_u8PinID*2));
+		/*Set type of output pin*/
+		WR_BIT(GPIOC->OTYPER,Copy_u8PinID,Copy_u8PinTYPES);
 
 		break;
 	}
@@ -59,72 +59,72 @@ void GPIO_voidInitOutputPin(u8 Copy_u8PortID,u8 Copy_u8PinID,u8 Copy_u8PinTYPES,
 /*ths fun is to initialize and pin to be in pin*/
 void GPIO_voidInitInputPin(u8 Copy_u8PortID,u8 Copy_u8PinID,u8 Copy_u8PinMode){
 	switch (Copy_u8PortID){
-		case GPIO_PORTA:
-			/*set the direction of the pin to be output*/
-			/*bit masking*/
-			GPIOA->ModeR &=~(3<<(Copy_u8PinID*2));
-			GPIOA->ModeR |=(0b00<<(Copy_u8PinID*2));
-			 WR_BIT(GPIOC->PUPDR,Copy_u8PinID,Copy_u8PinMode);
+	case GPIO_PORTA:
+		/*set the direction of the pin to be output*/
+		/*bit masking*/
+		GPIOA->ModeR &=~(3<<(Copy_u8PinID*2));
 
+		GPIOA->PUPDR&=~(3<<(Copy_u8PinID*2));
+		GPIOA->PUPDR&=~(Copy_u8PinMode<<(Copy_u8PinID*2));
 
-			break;
-		case GPIO_PORTB:
-			/*set the direction of the pin to be output*/
-					/*bit masking*/
-					GPIOB->ModeR &=~(3<<(Copy_u8PinID*2));
-					GPIOB->ModeR |=(0b00<<(Copy_u8PinID*2));
-					 WR_BIT(GPIOC->PUPDR,Copy_u8PinID,Copy_u8PinMode);
+		break;
+	case GPIO_PORTB:
+		/*set the direction of the pin to be output*/
+		/*bit masking*/
+		GPIOB->ModeR &=~(3<<(Copy_u8PinID*2));
+		GPIOB->PUPDR&=~(3<<(Copy_u8PinID*2));
+		GPIOB->PUPDR&=~(Copy_u8PinMode<<(Copy_u8PinID*2));
 
-			break;
-		case GPIO_PORTC:
-			/*set the direction of the pin to be output*/
-					/*bit masking*/
-					GPIOC->ModeR &=~(3<<(Copy_u8PinID*2));
-					GPIOC->ModeR |=(0b00<<(Copy_u8PinID*2));
-					 WR_BIT(GPIOC->PUPDR,Copy_u8PinID,Copy_u8PinMode);
-			break;
-		}
+		break;
+	case GPIO_PORTC:
+		/*set the direction of the pin to be output*/
+		/*bit masking*/
+		GPIOC->ModeR &=~(3<<(Copy_u8PinID*2));
+		GPIOC->PUPDR&=~(3<<(Copy_u8PinID*2));
+		GPIOC->PUPDR&=~(Copy_u8PinMode<<(Copy_u8PinID*2));
+		break;
+	}
 }
 /*this fun is to set the value of any output pin[high/low]*/
 void GPIO_voidSetPinValue(u8 Copy_u8PortID,u8 Copy_u8PinID,u8 Copy_u8value){
 	switch (Copy_u8PortID){
-		case GPIO_PORTA:
-			/*set the direction of the pin to be output*/
-			/*bit masking*/
-			WR_BIT(GPIOA->ODR,Copy_u8PinID,Copy_u8value);
+	case GPIO_PORTA:
+		/*set the direction of the pin to be output*/
+		/*bit masking*/
+		WR_BIT(GPIOA->ODR,Copy_u8PinID,Copy_u8value);
 
 
-			break;
-		case GPIO_PORTB:
-			/*set the direction of the pin to be output*/
-					/*bit masking*/
-			WR_BIT(GPIOB->ODR,Copy_u8PinID,Copy_u8value);
-			break;
-		case GPIO_PORTC:
-			/*set the direction of the pin to be output*/
-					/*bit masking*/
-			WR_BIT(GPIOC->ODR,Copy_u8PinID,Copy_u8value);
-			break;
-		}
+		break;
+	case GPIO_PORTB:
+		/*set the direction of the pin to be output*/
+		/*bit masking*/
+		WR_BIT(GPIOB->ODR,Copy_u8PinID,Copy_u8value);
+		break;
+	case GPIO_PORTC:
+		/*set the direction of the pin to be output*/
+		/*bit masking*/
+		WR_BIT(GPIOC->ODR,Copy_u8PinID,Copy_u8value);
+		break;
+	}
 }
 void GPIO_void_8_PinValue(u8 Copy_u8PortID,u8 Copy_u8PinsStarts,u8 Copy_u8value){
-switch(Copy_u8PortID){
-case GPIO_PORTA:
-			/*set the VALue of the pin to be HIGH OR LOW*/
-			/*bit masking*/
-	        GPIOA->ODR&=~(0x7F<<(Copy_u8PinsStarts*1));
-			GPIOA->ODR|=Copy_u8value;
+	switch(Copy_u8PortID){
+	case GPIO_PORTA:
+		/*set the VALue of the pin to be HIGH OR LOW*/
+		/*bit masking*/
+		GPIOA->ODR&=~(0x7F<<(Copy_u8PinsStarts*1));
+		GPIOA->ODR|=Copy_u8value;
 
 
-			break;
-		case GPIO_PORTB:
-			/*set the VALue of the pin to be HIGH OR LOW*/
-					/*bit masking*/
-			GPIOB->ODR&=~(0x7F<<(Copy_u8PinsStarts*1));
-						GPIOB->ODR|=Copy_u8value;
-			break;
+		break;
+	case GPIO_PORTB:
+		/*set the VALue of the pin to be HIGH OR LOW*/
+		/*bit masking*/
+		GPIOB->ODR&=~(0x7F<<(Copy_u8PinsStarts*1));
+		GPIOB->ODR|=Copy_u8value;
+		break;
 
-		}
+	}
 
 
 
@@ -138,7 +138,7 @@ u8 GPIO_u8GetPinValue(u8 Copy_u8PortID,u8 Copy_u8PinID){
 	switch(Copy_u8PortID){
 	case GPIO_PORTA:
 		Local_Return=GET_BIT(GPIOA->IDR,Copy_u8PinID);
-	break;
+		break;
 	case GPIO_PORTB:
 		Local_Return=GET_BIT(GPIOB->IDR,Copy_u8PinID);
 		break;
@@ -150,22 +150,22 @@ u8 GPIO_u8GetPinValue(u8 Copy_u8PortID,u8 Copy_u8PinID){
 }
 void GPIO_voidTGLPin(u8 Copy_u8PortID,u8 Copy_u8PinID){
 	switch (Copy_u8PortID){
-			case GPIO_PORTA:
-				/*set the direction of the pin to be output*/
-				/*bit masking*/
-				TGL_BIT(GPIOA->ODR,Copy_u8PinID);
+	case GPIO_PORTA:
+		/*set the direction of the pin to be output*/
+		/*bit masking*/
+		TGL_BIT(GPIOA->ODR,Copy_u8PinID);
 
 
-				break;
-			case GPIO_PORTB:
-				/*set the direction of the pin to be output*/
-						/*bit masking*/
-				TGL_BIT(GPIOB->ODR,Copy_u8PinID);
-				break;
-			case GPIO_PORTC:
-				/*set the direction of the pin to be output*/
-						/*bit masking*/
-				TGL_BIT(GPIOC->ODR,Copy_u8PinID);
-				break;
-			}
+		break;
+	case GPIO_PORTB:
+		/*set the direction of the pin to be output*/
+		/*bit masking*/
+		TGL_BIT(GPIOB->ODR,Copy_u8PinID);
+		break;
+	case GPIO_PORTC:
+		/*set the direction of the pin to be output*/
+		/*bit masking*/
+		TGL_BIT(GPIOC->ODR,Copy_u8PinID);
+		break;
+	}
 }
