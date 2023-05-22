d2cd5d8e5ffccb641b1ec387d0ca20f672c030b2
Mohamed Ashraf <92621216+MoAshraf259@users.noreply.github.com>
2023-05-22 21:59:51 +0300

    UPDATED MCU SPECIFIC HEADER FILE WITH NEW ADDR

--- RCC/stm32f446.h
+++ RCC/stm32f446.h
@@ -11,6 +11,12 @@
 #include <stdint.h>
 #define __vo volatile
 
+#define SET		1
+#define RESET	0
+#define ENABLE	SET
+#define DISABLE	RESET
+#define HIGH	ENABLE
+#define LOW		DISABLE
 /*
  * AHBx and APBx Bus Peripheral base addresses
  */
@@ -38,17 +44,22 @@
 #define RCC_BASEADDR                     (AHB1PERIPH_BASEADDR + 0x3800)
 
 
+#define NVIC_BASEADDR					 (0xE000E100)
+#define EXTI_BASEADDR					 (0x40013C00)
+
+#define SYSCFG_BASEADDR					(0x40013800)
+
 typedef struct
 {
-	__vo uint32_t MODER;                        /*!< GPIO port mode register,                    	Address offset: 0x00      */
-	__vo uint32_t OTYPER;                       /*!<  ,     										Address offset: 0x04      */
-	__vo uint32_t OSPEEDR;
+	__vo uint32_t MODER;                 /*!< GPIO port mode register,      Address offset: 0x00      */
+	__vo uint32_t OTYPER;                /*!<  TYPE OF OUTPUT PINS     		Address offset: 0x04      */
+	__vo uint32_t OSPEEDR;					/*	*/
 	__vo uint32_t PUPDR;
 	__vo uint32_t IDR;
 	__vo uint32_t ODR;
 	__vo uint32_t BSRR;
 	__vo uint32_t LCKR;
-	__vo uint32_t AFR[2];					 /*!< AFR[0] : GPIO alternate function low register, AF[1] : GPIO alternate function high register    		Address offset: 0x20-0x24 */
+	__vo uint32_t AFR[2];				/*!< AFR[0] : GPIO alternate function low register, AF[1] : GPIO alternate function high register    		Address offset: 0x20-0x24 */
 }GPIO_RegDef_t;
 
 /*
@@ -93,6 +104,37 @@ typedef struct
 
 } RCC_RegDef_t;
 
+typedef struct{
+	__vo uint32_t ISER[32];
+	__vo uint32_t ICER[32];
+	__vo uint32_t ISPR[32];
+	__vo uint32_t ICPR[32];
+	__vo uint32_t IABR[64];
+	__vo uint32_t IPR[60];
+}NVIC_RegDef_t;
+
+typedef struct{
+	__vo uint32_t IMR;
+	__vo uint32_t EMR;
+	__vo uint32_t RTSR;
+	__vo uint32_t FTSR;
+	__vo uint32_t SWIER;
+	__vo uint32_t PR;
+}EXTI_RegDef_t;
+
+
+typedef struct{
+	__vo uint32_t SYSCFG;
+	__vo uint32_t PMC;
+	__vo uint32_t EXTICR[4];
+	__vo uint32_t CMPCR;
+	__vo uint32_t CFGR;
+}SYSCFG_RegDef_t;
+
+#define SYSCFG				((SYSCFG_RegDef_t*)SYSCFG_BASEADDR)
+#define EXTI				((EXTI_RegDef_t*)EXTI_BASEADDR)
+#define NVIC				((NVIC_RegDef_t*)NVIC_BASEADDR)
+
 
 #define GPIOA  				((GPIO_RegDef_t*)GPIOA_BASEADDR)
 #define GPIOB  				((GPIO_RegDef_t*)GPIOB_BASEADDR)
@@ -105,6 +147,36 @@ typedef struct
 
 #define RCC 				((RCC_RegDef_t*)RCC_BASEADDR)
 
+#define RCB_AIRCR			(*((__vo uint32_t*)0xE000ED00+0x0C))
+
+#define GPIO_PIN_NO_0		0
+#define GPIO_PIN_NO_1		1
+#define GPIO_PIN_NO_2		2
+#define GPIO_PIN_NO_3		3
+#define GPIO_PIN_NO_4		4
+#define GPIO_PIN_NO_5		5
+#define GPIO_PIN_NO_6		6
+#define GPIO_PIN_NO_7		7
+#define GPIO_PIN_NO_8		8
+#define GPIO_PIN_NO_9		9
+#define GPIO_PIN_NO_10		10
+#define GPIO_PIN_NO_11		11
+#define GPIO_PIN_NO_12		12
+#define GPIO_PIN_NO_13		13
+#define GPIO_PIN_NO_14		14
+#define GPIO_PIN_NO_15		15
+
+#define GPIO_PORT_TO_NUMBER(x)		((x==GPIOA)? 0:\
+									(x==GPIOB)? 1:\
+									(x==GPIOC)?	2:\
+									(x==GPIOD)?	3:\
+									(x==GPIOE)?	4:\
+									(x==GPIOF)?	5:\
+									(x==GPIOG)?	6:\
+									(x==GPIOH)?	7:0)
+
 #include "stm32f446_rcc.h"
 #include "stm32f446_gpio.h"
+#include "stm32f446_nvic.h"
+#include "stm32f446_exti.h"
 #endif /* STM32F446_H_ */
